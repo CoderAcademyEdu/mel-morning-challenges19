80183436c6e21eaec8678fa2f45779eae131a353
anharathoit <anharzihan@gmail.com>
2019-08-25 19:38:12 +1000

    Added bill calculator challenge

---
+++ term_one/ruby/week_2/1_bill_calculator.rb
@@ -0,0 +1,26 @@
+# A group of friends have just finished lunch. Write method to work out how much each person owes.
+
+# - Your app should ask "Enter a customer's name"- Your first line of code is:
+# items = [
+#     {
+#         customer: "John",
+#         item: "Soup",
+#         cost: 8.50
+#     },
+#     {
+#         customer: "Sarah",
+#         item: "Pasta",
+#         cost: 12
+#     },
+#     {
+#         customer: "John",
+#         item: "Coke",
+#         cost: 2.50
+#     }
+# ]
+
+# - Your app should output, for example: "John owes $11.0"
+
+## Beast
+# Format your total so it always shows two decimal places, e.g. $11.00
+# Allow items to be added to the list. E.g. "Press 1 to add items, press 2 to work out a total".
