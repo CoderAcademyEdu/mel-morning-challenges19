2dc997ebdb5030af0a4a4ca646c2c38151a9cba1
Harrison Malone <harrison.malone2@gmail.com>
2019-09-01 21:09:12 +1000

    morning challenges week 3

---
+++ term_one/ruby/week_3/1_tinder.rb
@@ -0,0 +1,7 @@
+# Core
+
+# The year is 2025, and Tinder is running the world. Create a method, that will take an array of 5 names, entered by the user, and will use a ruby method to print out a random person from the array that the user has been matched with
+
+# Advanced
+
+# Add in functionality to allow the user to pick ‘yes’ or ’no’ to their match. If they pick ‘yes’, give them a congratulatory message telling them who their date is. If they say no, chose another person from the array. Eventually, if there is no one left in the array, let the user know that they have run out of swipes
---
+++ term_one/ruby/week_3/2_party_parrot_cocktails_version_two.rb
@@ -0,0 +1,26 @@
+## 04/03/2019
+## the party parrot problem, again!
+
+# 1. Use CASE statement to rewrite the problem below to repeatedly take orders for drinks. Start all the drink numbers at 0, and when they reach 10 drinks in total, inform the user that it's time to go home. 
+
+  # you are working at a bar where you have a current backlog of drinks to make:
+  # 3 party parrot cocktails
+    # cocktails sell for $22, and cost $8 to make
+
+  # 2 party parrot waters
+    # beer sell for $12, and cost $3 to pour
+
+  # 6 party parrot beers
+    # water sell for $6, and cost $0.15 to mak
+  # ****
+  # write a program that asks the customer for their order.
+  # if they order a party parrot cocktail, add one to the number of party parrot cocktails you need to make,
+  # if they order a party parrot water, add one to the number of party parrot waters you need to make,
+  # if they order a party parrot beer, add one to the number of party parrot beers you need to pour
+  # print the final drinks order so you know what to make
+
+
+# 2. Add to your program a question regarding how much money the user has. When they have blown their budget, inform the user and tell them to head home.
+
+# *BEAST MODE*
+# 3. When the user has more than 3/4 of their money left, inform them in green output of the amount remaining (using colorize). If they have 1/2 to 3/4 use blue, 1/4 to a half, yellow, and less than 1/4 left use red.
