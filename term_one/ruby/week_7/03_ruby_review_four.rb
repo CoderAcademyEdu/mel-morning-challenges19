5a695f583b636b76715e664471a6d43fac91c2ac
anharathoit <anharzihan@gmail.com>
2019-10-04 09:15:01 +1000

    Added ruby review challenge

---
+++ term_one/ruby/week_7/03_ruby_review_four.rb
@@ -0,0 +1,18 @@
+# 1. 
+# Write a ruby method that takes one paramter, rows, and draws a triangle over that many rows.
+# The following would be triangle over four rows
+# *
+# **
+# ***
+# ****
+
+# Write code to prompt the user to enter how many rows
+
+# Add a method to draw a square and a rectangle
+# Prompt the user to choose which shape and enter values required
+
+# 2.
+# Write a method that simulates rolling two 6 sided dice and calculating the sum of the numbers
+# You program should roll the dice 36,000 times
+# Use an array to tally the number of times each possible sum appears
+# Display the results
