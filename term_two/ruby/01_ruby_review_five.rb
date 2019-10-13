c26254911f01ee138313bbd2bc2456850b43a49f
anharathoit <anharzihan@gmail.com>
2019-10-14 08:56:19 +1100

    Term-2 w1d1 challenge uploaded M02019

---
+++ term_two/ruby/01_ruby_review_five.rb
@@ -0,0 +1,10 @@
+# 1. 
+# Write a ruby method that returns the smallest of three floating point numbers
+
+# 2.
+# Write a ruby method that takes an integer and returns the number with its digits reversed.
+# For example, given the number 7631, the function should return 1367
+
+# 3.
+# The greatest common divisor (GCD) of two integers is the largest integer that evenly divides each of the numbers.
+# Write a method gcd that returns the greatest common divisor of two integers.
