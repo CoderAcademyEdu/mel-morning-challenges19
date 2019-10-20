02b9661fe1d2c9b37c2b1ef047a984de5507c63c
anharathoit <anharzihan@gmail.com>
2019-10-20 19:41:45 +1100

    Added scrabble challenge

---
+++ term_two/ruby/week_2/01_scrabble.rb
@@ -0,0 +1,8 @@
+## Morning Challenge
+
+# The year is 2021, and theres been a new version of scrabble created. This version is much more simple. The points are scored according to the letters position in the alphabet. a = 1. b = 2, c = 3....etc. With the highest word winnning that round.
+# Create a method that will receive a string of one or many words, and returns the highest scoring word. 
+# If two words score the same, return the one that appears first.
+# You will only receive downcase alphanumeric characters (a-z).
+# * **E.g.** word_ranking('hi there') -> 'there'
+# *Explanation* 'hi' is worth 17, as 'there' is worth 56, so return 'there'
