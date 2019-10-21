09fab7529545d03b3c4082db2eb1e7bceccaf54c
anharathoit <anharzihan@gmail.com>
2019-10-21 22:35:39 +1100

    Added new challenge

---
+++ term_two/ruby/week_2/02_capital_letters.rb
@@ -0,0 +1,6 @@
+## Morning Challenge
+
+# Create a method that will take a single string as input, and will return an ordered array containing the index of all capital letters. Return an empty array if no capitals are found. There may be some special characters included
+# E.g. capital_index("rABbiT") -> [1, 2, 5]
+# E.g. capital_index("lowercase") -> [ ]
+# E.g. capital_index("HELLo!"] -> [0, 1, 2, 3]
