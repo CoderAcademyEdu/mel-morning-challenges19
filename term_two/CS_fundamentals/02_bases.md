a0b6ad07d5188f8abdeb42e74df4ac0444eda615
anharathoit <anharzihan@gmail.com>
2019-10-25 08:25:23 +1100

    Added challenges

---
+++ term_two/CS_fundamentals/02_bases.md
@@ -0,0 +1,12 @@
+# Morning Challenge
+
+- Convert 0xA3B to binary
+- Convert 0xDA to binary
+- What is 0xB in decimal
+- What is 0xAE in decimal
+- Convert 157 in base-10 to binary and hex
+- What are the three numbers after 9 in base 12
+- What are the five numbers after 9 in base 13
+- Convert 001101001 to hexadecimal
+- is 101110 odd or even?
+- is 100111 odd or even?
---
+++ term_two/ruby/week_1/04_basic_toy_robot.rb
@@ -0,0 +1,27 @@
+# in this challenge we'll be implementing a basic version of the toy robot challenge
+
+# watch this video to see the end product i'd like you to achieve https://www.youtube.com/watch?v=H-xmoKu4SUw&feature=youtu.be
+
+# given this array
+
+table = [
+  [" ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " "],
+]
+
+# place our robot "🤖" on the table using gets.chomp for the X, Y position
+
+# using a while loop allow the user to choose whether they want to move north, south, east or west (again using gets.chomp)
+
+# allow the user to quit (end the loop) or keep playing the game
+
+# optional
+
+# ensure that a user can't go out of the 5 by 5 bounds of the board
+
+# handle all edge cases with user input
+
+# consider using a Robot class to implement the same logic
